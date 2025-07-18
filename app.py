d02d483c951eadd0a21626cef6b77ecff6aa9966
adityaprog <aditraj964@gmail.com>
2025-07-18 20:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello, World!</h1>"
+    return '''<h1>Hello!</h1>
+            <p>From Aditya to Siddharth and all my dear users.</p>
+            <p>Welcome to my Flask app.</p>'''
 
 
 if __name__ == '__main__':
