1c20664696185bb357b524814768c1824f105f70
adityaprog <aditraj964@gmail.com>
2025-07-20 11:49:01 +0530

    update app and html

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ home_title ="Flask App"
 def home_page():
     return render_template("index.html", flask_title= home_title)
 
+@app.route('/<page>')
+def render_page(page):
+    return render_template(f"{page}.html", flask_title=f"{home_title} - {page.capitalize()}")
+
 
 if __name__ == '__main__':
     app.run()
