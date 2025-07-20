3c94002b286aa0d1a160f8b356135a237d311dbf
adityaprog <aditraj964@gmail.com>
2025-07-20 14:41:04 +0530

    big updates

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, abort
+import os
 
 app = Flask(__name__)
 
-# Variables
+# Title shown in <title>
+home_title = "Flask App"
 
-home_title ="Flask App"
+# Path to templates folder
+template_dir = os.path.join(app.root_path, 'templates')
 
 
 @app.route('/')
 def home_page():
-    return render_template("index.html", flask_title= home_title)
+    return render_template("index.html", flask_title=home_title)
 
-@app.route('/<page>')
+
+@app.route('/<page>/')
 def render_page(page):
-    return render_template(f"{page}/index.html", flask_title=f"{home_title} - {page.capitalize()}", heading = page)
+    template_path = f"{page}/index.html"
+    full_path = os.path.join(template_dir, template_path)
+
+    if os.path.exists(full_path):
+        return render_template(
+            template_path,
+            flask_title=f"{home_title} - {page.capitalize()}",
+            heading=page
+        )
+    else:
+        # Page doesn't exist, show custom 404
+        abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404/index.html", flask_title="Page Not Found"), 404
 
 
 if __name__ == '__main__':
